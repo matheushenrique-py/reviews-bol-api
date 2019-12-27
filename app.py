0493d32fb68ebec15328dc9f04d24e0c28d22f80
matheushenrique-py <matheushenrique.py@gmail.com>
2019-12-27 09:22:01 -0300

    criação da API flask

--- app.py
+++ app.py
@@ -1,62 +1,77 @@
 # coding: utf-8
-
 import re
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
-df = pd.read_excel('data/testes_novembro.xlsx')
-df = df.loc[:, ['STATUS', 'NAME']]
+app = Flask(__name__)
+api = Api(app)
 
-df.columns = ['NAME', 'COMMENTS']
+app.config['JSON_AS_ASCII'] = False
 
-N_REPETICAO_PALAVRAS = 3
-LEN_PALAVRAS_IGNORADAS = 2
 
-atrib = []
-ind_reprovados = np.zeros(len(df))
-for i in range(len(df)):
+class Filtros(Resource):
 
-    # juntando título e comentário
-    atrib.append(str(df.NAME.values[i]).lower() + ' ' + str(df.COMMENTS.values[i]).lower())
+    def post(self):
+        N_REPETICAO_PALAVRAS = 3
 
-    # procura comentários repetidos
-    j = 0
-    repets = df.duplicated(subset='COMMENTS')
-    for repetido in repets:
-        if repetido:
-            ind_reprovados[j] = 1
-        j += 1
+        arq = request.get_json(force=True)
+        df = pd.DataFrame(arq)
 
-    # procurando palavrões
-    proibidas = []
-    with open('data/bad_words.txt', 'r') as file:
-        words = file.read()
+        atrib = []
+        ind_reprovados = np.zeros(len(df))
 
-        for palavra in words.split():
-            proibidas.append(palavra)
+        for i in range(len(df)):
 
-    for palavra in atrib[i].split():
-        if palavra in proibidas:
-            ind_reprovados[i] = 1
+            # juntando título e comentário
+            atrib.append(str(df.NAME.values[i]).lower() + ' ' + str(df.COMMENTS.values[i]).lower())
 
-    # procurando palavras repetidas
-    palavras_comuns = [' a ', ' o ', ' e ', ' um ', ' uma ', ' ele ', ' ela ', ' com ', ' de ', ' da ', ' do ']
-    for palavra in palavras_comuns:
-        atrib[i] = atrib[i].replace(palavra, ' ')
+            # procura comentários repetidos
+            j = 0
+            repets = df.duplicated(subset='COMMENTS')
+            for repetido in repets:
+                if repetido:
+                    ind_reprovados[j] = 1
+                j += 1
 
-    try:
-        count = CountVectorizer().fit_transform([atrib[i]])
-        n = count.toarray().max()
-    except ValueError:
-        ind_reprovados[i] = 1
-    if n >= N_REPETICAO_PALAVRAS:
-        ind_reprovados[i] = 1
+            # procurando palavrões
+            proibidas = []
+            with open('data/bad_words.txt', 'r', encoding='utf8') as file:
+                words = file.read()
 
-    # outros padrões indesejados
-    regex = re.compile(r'\d{4,}|[^aeiou .,áàãâéêíóôú0-9]{5,}|[#$&*/\\:;<=>@\[\]{|}~^]')
-    if regex.search(atrib[i]) is not None:
-        ind_reprovados[i] = 1
-df['saida_show'] = ind_reprovados
+                for palavra in words.split():
+                    proibidas.append(palavra)
 
-df.to_excel('saida.xlsx')
+            for palavra in atrib[i].split():
+                if palavra in proibidas:
+                    ind_reprovados[i] = 1
+
+            # procurando palavras repetidas
+            palavras_comuns = [' a ', ' o ', ' e ', ' um ', ' uma ', ' ele ', ' ela ', ' com ', ' de ', ' da ', ' do ']
+            for palavra in palavras_comuns:
+                atrib[i] = atrib[i].replace(palavra, ' ')
+
+            try:
+                count = CountVectorizer().fit_transform([atrib[i]])
+                n = count.toarray().max()
+            except ValueError:
+                ind_reprovados[i] = 1
+            if n >= N_REPETICAO_PALAVRAS:
+                ind_reprovados[i] = 1
+
+            # outros padrões indesejados
+            regex = re.compile(r'\d{4,}|[^aeiou .,áàãâéêíóôú0-9]{5,}|[#$&*/\\:;<=>@§£¢¬©\[\]{|}~^]')
+            if regex.search(atrib[i]) is not None:
+                ind_reprovados[i] = 1
+
+        return {'id': list(df.index), 'STATUS': ind_reprovados.tolist()}
+
+
+api.add_resource(Filtros, '/filtro')
+
+if __name__ == '__main__':
+    app.run(debug=True)
